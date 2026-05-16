8195873769c47590ddd8e07818439e75f8ff224a
Natividade0 <86794017+Natividade0@users.noreply.github.com>
2026-05-16 09:22:37 -0300

    Add automatic APR models

---
+++ app/src/main/java/com/safefield/app/AprModels.kt
@@ -0,0 +1,13 @@
+package com.safefield.app
+
+data class AprItem(
+    val activity: String,
+    val danger: String,
+    val risk: String,
+    val consequence: String,
+    val control: String,
+    val epi: String,
+    val severity: Int,
+    val probability: Int,
+    val classification: String
+)
